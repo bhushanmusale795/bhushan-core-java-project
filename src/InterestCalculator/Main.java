2297cb1371f2e334953d8823bd95b6828442f1c7
bhushanmusale795 <98394603+bhushanmusale795@users.noreply.github.com>
2022-05-10 20:23:38 +0530

    core java project

---
+++ src/InterestCalculator/Main.java
@@ -0,0 +1,38 @@
+package InterestCalculator;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		
+	    Scanner input = new Scanner(System.in);
+
+	   
+	    System.out.print("Enter the principal: ");
+	    double principal = input.nextDouble();
+
+	    System.out.print("Enter the rate: ");
+	    double rate = input.nextDouble();
+
+	    System.out.print("Enter the time: ");
+	    double time = input.nextDouble();
+	    
+	    System.out.print("Enter number of times interest is compounded: ");
+	    int number = input.nextInt();
+
+	    double interest = (principal * time * rate) / 100;
+
+	    double interest1 = principal * (Math.pow((1 + rate/100), (time * number))) - principal;
+
+	    System.out.println("Principal: " + principal);
+	    System.out.println("Interest Rate: " + rate);
+	    System.out.println("Time Duration: " + time);
+	    System.out.println("Simple Interest: " + interest);
+	    System.out.println("Compound Interest: " + interest1);
+
+	    input.close();
+	  }
+	
+
+	}
+
+
